aa9812b88a4143878fb6c609f5d167c9c82bda78
Fernando Warno <ferwar@hotmail.com>
2022-09-27 10:08:10 -0300

    Creo UserList e importo a App.js

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,8 +1,13 @@
 import Navbar from "./components/Navbar";
+import UserList from "./components/UserList";
+
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <UserList />
+    </>
   );
 }
 
--- react-redux-app/src/components/UserList.js
+++ react-redux-app/src/components/UserList.js
@@ -1,41 +1,58 @@
-import React from 'react'
+import React from "react";
 
 const UserList = () => {
-
-    const users =  [
-        {
-            id: 1,
-            email: "george.bluth@reqres.in",
-            first_name: "George",
-            last_name: "Bluth",
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
-        },
-        {
-            id: 2,
-            email: "janet.weaver@reqres.in",
-            first_name: "Janet",
-            last_name: "Weaver",
-            avatar: "https://reqres.in/img/faces/2-image.jpg"
-        },
-        {
-            id: 3,
-            email: "emma.wong@reqres.in",
-            first_name: "Emma",
-            last_name: "Wong",
-            avatar: "https://reqres.in/img/faces/3-image.jpg"
-        },
-        {
-            id: 4,
-            email: "eve.holt@reqres.in",
-            first_name: "Eve",
-            last_name: "Holt",
-            avatar: "https://reqres.in/img/faces/4-image.jpg"
-        }
-    ]
+  const users = [
+    {
+      id: 1,
+      email: "george.bluth@reqres.in",
+      first_name: "George",
+      last_name: "Bluth",
+      avatar: "https://reqres.in/img/faces/1-image.jpg",
+    },
+    {
+      id: 2,
+      email: "janet.weaver@reqres.in",
+      first_name: "Janet",
+      last_name: "Weaver",
+      avatar: "https://reqres.in/img/faces/2-image.jpg",
+    },
+    {
+      id: 3,
+      email: "emma.wong@reqres.in",
+      first_name: "Emma",
+      last_name: "Wong",
+      avatar: "https://reqres.in/img/faces/3-image.jpg",
+    },
+    {
+      id: 4,
+      email: "eve.holt@reqres.in",
+      first_name: "Eve",
+      last_name: "Holt",
+      avatar: "https://reqres.in/img/faces/4-image.jpg",
+    },
+  ];
 
   return (
-    <div>UserList</div>
-  )
-}
+    <div className="container">
+      {/* En cada columna de la row hay una tarjeta con un usuario */}
+      <div className="row">
+        {/* Necesito iterar a los users usando la funcion map que dentro
+            tiene una funcion con un return implicito y dentro un div, recibe
+            a un usuario y un index que lo utilizo para el key*/}
+        {users.map((user, index) => (
+          <div key={index} className="col-md-3">
+            <div className="card">
+              <img src={user.avatar} alt="avatar" />
+              <div className="card-body">
+                <h5 className="card-title">{`${user.first_name} ${user.last_name}`}</h5>
+                <p className="card-text">{user.email}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default UserList
+export default UserList;
